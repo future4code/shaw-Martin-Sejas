abb62aa528b22b2072dd2a4cbdbebe00f2f660d7
martinsejas <martinhugosejas@gmail.com>
2022-03-22 16:55:34 -0300

    copy project into another folder

---
+++ Modulo2/props-labedin/labedin2/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from 'styled-components';
+
+const SmallCardContainer = styled.div
+`
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border: 1px black solid;
+
+    img {
+        width: 3vw;
+        padding: 1%; 
+    }
+
+    div {
+        padding-left: 3%; 
+    }
+
+`
+
+
+function CardPequeno(props) {
+
+
+    return (
+        <SmallCardContainer>
+            <img src={props.imagem} />
+            <div>
+                <p> <b>{props.infoType}: </b> {props.userInfo}</p>
+            </div>
+        </SmallCardContainer>
+
+    )
+
+}
+
+export default CardPequeno;
