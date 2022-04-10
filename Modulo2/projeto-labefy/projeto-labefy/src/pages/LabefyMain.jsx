5c3d9189d05da7ad03a671e95c55394744e8084c
martinsejas <martinhugosejas@gmail.com>
2022-04-10 17:20:17 -0300

    removed some discrepancies

--- Modulo2/projeto-labefy/projeto-labefy/src/pages/LabefyMain.jsx
+++ Modulo2/projeto-labefy/projeto-labefy/src/pages/LabefyMain.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import axios from "axios";
 import { COLORS } from "../constants/styling";
 import Playlists from "./Playlists";
 import PlaylistContent from "./PlaylistContent";
