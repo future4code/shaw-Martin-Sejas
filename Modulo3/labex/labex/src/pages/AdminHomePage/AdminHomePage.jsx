1b50932e9050d5545889c933c064209d52636c79
martinsejas <martinhugosejas@gmail.com>
2022-04-27 19:30:14 -0300

    painel administrativo desktop quase completo

--- Modulo3/labex/labex/src/pages/AdminHomePage/AdminHomePage.jsx
+++ Modulo3/labex/labex/src/pages/AdminHomePage/AdminHomePage.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { goToHomePage, goToLastPage, goToLoginPage } from '../../services/Routes/coordinator';
-import { AdminHomePageDiv } from './styled';
+import { goToHomePage, goToLastPage, goToLoginPage, goToCreateTripPage } from '../../services/Routes/coordinator';
+import { AdminHomePageDiv, AdminHomePageCoreDiv, MissionNameList } from './styled';
 import Header from '../../components/Header/Header';
 import { Button } from '@chakra-ui/react';
 import { LoggedIn } from '../../components/hooks/LoggedIn';
+import { useRequestData } from '../../services/requests';
+import TripCard from '../../components/TripCard/TripCard';
 
 function AdminHomePage() {
+  
   const navigate = useNavigate();
   
 
@@ -19,23 +22,38 @@ function AdminHomePage() {
 
   },[])
 
+  const trips = useRequestData("trips");
  
-  
+ 
 
   const logOut = () => {
     window.localStorage.clear(); 
     goToHomePage(navigate); 
   }
+
+  let showTrips = (trips && trips.trips.map( (trip) => {
+    console.log(trip)
+    return(<TripCard key= {trip.id} name={trip.name} id={trip.id} />)
+  }))
+
+
   return (
     <AdminHomePageDiv>
     <Header left = "Voltar" leftButton={() => goToLastPage(navigate)}
             right = "Logout" rightButton={() =>{logOut()}}/>
 
-           <div>
+           <AdminHomePageCoreDiv>
              
-             <p>AdminHome (AdminHomePage)</p>
+             <h1>Painel Administrativo</h1>
+
+             <Button colorScheme="secondary" onClick={() => { goToCreateTripPage(navigate)}}>Criar Viagem</Button>
+
+             <MissionNameList>
+                {showTrips}
+              </MissionNameList>
+             
             
-           </div>
+           </AdminHomePageCoreDiv>
     </AdminHomePageDiv>
   
   )
--- Modulo3/labex/labex/src/pages/AdminHomePage/styled.js
+++ Modulo3/labex/labex/src/pages/AdminHomePage/styled.js
@@ -7,13 +7,52 @@ export const AdminHomePageDiv = styled.div`
     grid-template-rows: ${HEADER.size} 1fr; 
     background-color: ${COLORS.background}; 
     font-family: ${MYFONTS.title};
-
-    div {
-        display: flex; 
-        justify-content: center; 
-        align-items: center; ; 
-        color: ${COLORS.font}; 
-        font-size: 2rem; 
-    }
     
+`
+export const AdminHomePageCoreDiv = styled.div`
+
+    display: flex; 
+    flex-direction: column; 
+    color: ${COLORS.font}; 
+
+    align-items: center;
+    margin-top: 4%; 
+
+    @media(max-width:750px) {
+            margin-top: 25%;
+        }
+
+    h1{
+        margin-bottom: 2.5rem;
+        font-size: 3rem; 
+
+        @media(max-width:750px) {
+            font-size: 2.5rem;  
+            margin-bottom: 2rem;
+        }
+    }
+
+    Button {
+    margin-top: 0.5rem;
+    font-size: 1.75rem;
+    padding: 1.5% 1.5%;
+
+    @media(max-width:750px) {
+        
+        font-size: 1.25rem;
+        padding: 1.5% 5%;
+    }
+
+}
+
+`
+
+export const MissionNameList = styled.div`
+    display: flex; 
+    flex-direction: column; 
+    margin-top: 3%; 
+    overflow-y: auto; 
+    width: 40%; 
+    
+   
 `
