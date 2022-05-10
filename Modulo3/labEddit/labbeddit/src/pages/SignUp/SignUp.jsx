4fc57184d454797f977f4460d4be480e429181fb
martinsejas <martinhugosejas@gmail.com>
2022-05-10 17:16:57 -0300

    termino de layout de signup

--- Modulo3/labEddit/labbeddit/src/pages/SignUp/SignUp.jsx
+++ Modulo3/labEddit/labbeddit/src/pages/SignUp/SignUp.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Header from '../../components/Header/Header'
 import { goToLastPage } from '../../services/Routes/coordinates';
-import { SignUpContentDiv, SignUpMainDiv } from './styled'
+import { SignUpContentDiv, SignUpMainDiv, SignUpFormDiv } from './styled';
+import { Button, Input} from '@chakra-ui/react';
+import {FormControl, FormErrorMessage, Checkbox} from '@chakra-ui/react'
+import {Formik, Form, Field} from 'formik';
+import * as Yup from 'yup';
+
 
 function SignUp() {
   const navigate = useNavigate(); 
@@ -15,6 +20,86 @@ function SignUp() {
       rightButtonClick = {()=> goToLastPage(navigate)}
       />
       <SignUpContentDiv>
+        <h1>Olá, boas vindas ao LabEddit ;)</h1>
+
+        <SignUpFormDiv>
+      <Formik
+        initialValues={ {username: "", email:"", password: ""}}
+
+        //input control
+        validationSchema = { Yup.object({
+          username: Yup.string()
+          .min(4, "Minimo 4 caracteres")
+          .max(15, "Maximo 15 caracteres")
+          .required("Campo Obrigatorio"),
+          email: Yup.string()
+          .email("Usuario deve ser um email")
+          .min(4, "Minimo 4 caracteres")
+          .required("Campo Obrigatorio"),
+          password: Yup.string()
+          .max(30, 'Maximo 30 caracteres')
+          .min(8, "Minimo 8 caracteres")
+          .required('Campo Obrigatorio'),
+         
+        })}
+
+        //actions on Submit
+        onSubmit = { (values, actions) => {
+          setTimeout( () => {
+
+
+            actions.resetForm(); 
+            actions.setSubmitting(false);
+           
+            
+
+          }, 2000)
+        }}
+      >
+        { (props) => {
+          return (
+            <Form>
+            
+              <Field name="username">
+                {({field, form}) => ( 
+                  <FormControl isInvalid={form.errors.username && form.touched.username} className="field">
+                    <Input {...field} className="loginInput"  placeholder='Nome  de usuário' _placeholder={{opacity: 0.8, color: '#323941' }} />
+                    <FormErrorMessage className = "errorMessage">{form.errors.username}</FormErrorMessage>
+                  </FormControl>
+                  )}
+              </Field>
+
+              <Field name="email">
+                {({field, form}) => ( 
+                  <FormControl isInvalid={form.errors.email && form.touched.email} className="field">
+                    <Input {...field} className="loginInput"  placeholder='E-mail' _placeholder={{opacity: 0.8, color: '#323941' }} />
+                    <FormErrorMessage className = "errorMessage">{form.errors.email}</FormErrorMessage>
+                  </FormControl>
+                  )}
+              </Field>
+
+              <Field name="password">
+                {({field, form}) => ( 
+                  <FormControl isInvalid={form.errors.password && form.touched.password} className="field">
+                    <Input {...field} className="loginInput" placeholder='Senha' _placeholder={{opacity: 0.8, color: '#323941' }} type="password"/>
+                    <FormErrorMessage className = "errorMessage">{form.errors.password}</FormErrorMessage>
+                  </FormControl>
+                  )}
+              </Field>
+
+              <p>Ao continuar, você concorda com o nosso <span>Contrato de usuário </span>e nossa <span>Política de Privacidade.</span></p>
+              <Checkbox> <span id = 'checkbox'>Eu concordo em receber emails sobre coisas legais no Labeddit</span></Checkbox>
+              <Button id="loginButton" variant='solid' isLoading={props.isSubmitting} type='submit' >Cadastrar</Button>
+
+              
+            </Form>
+          )
+        }}
+      </Formik>
+
+     
+    </SignUpFormDiv>
+
 
       </SignUpContentDiv>
     </SignUpMainDiv>
--- Modulo3/labEddit/labbeddit/src/pages/SignUp/styled.js
+++ Modulo3/labEddit/labbeddit/src/pages/SignUp/styled.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
-import { HEADER } from "../../constants/theme";
+import { COLORS, HEADER, MYFONTS } from "../../constants/theme";
+import "@fontsource/ibm-plex-sans/700.css";
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/500.css";
+
 
 export const SignUpMainDiv = styled.div`
 display: grid; 
@@ -9,8 +13,120 @@ grid-template-rows: ${HEADER.size} 1fr;
 
 export const SignUpContentDiv = styled.div`
 display: flex; 
+flex-direction: column; 
 height: auto; 
 min-height: 95vh; 
-background-color: yellow; 
+width: 85%;
+background-color: ${COLORS.background}; 
+
+margin: 0 auto; 
+
+h1 {
+    font-family: ${MYFONTS.title}; 
+    color: ${COLORS.font.headings}; 
+    font-weight: 700; 
+    font-size: 33px; 
+    width: 100%;
+  
+    margin-top: 29px;
+    margin-bottom: 4vh; 
+
+    @media (min-height: 750px)
+    {
+      margin-bottom: 194px; 
+    }
+}
+
+`
+
+export const SignUpFormDiv = styled.div`
+display: flex; 
+flex-direction: column; 
+width: 100%; 
+height: auto; 
+
+@media (min-width: 750px)
+    {
+       justify-content: center;
+       align-items: center;
+      
+       margin-top: 20px; 
+    }
+
+
+p{
+    font-family: ${MYFONTS.body}; 
+    color: ${COLORS.font.subHeadings}; 
+    font-size: 14px; 
+    font-weight: 400;
+    max-width: 100%; 
+    margin-top: 30px; 
+    padding-left: 2px; 
+    margin-bottom: 17px; 
+
+    @media (min-height: 750px)
+    {
+     margin-top: 65px; 
+    }
+}
+
+    span{
+        display:inline; 
+        font-weight: 500; 
+        color: ${COLORS.font.Header}; 
+    }
+
+
+
+
+#checkbox {
+    font-family: ${MYFONTS.body}; 
+    color: ${COLORS.font.subHeadings}; 
+    font-size: 14px; 
+    font-weight: 400;
+    max-width: 100%; 
+   
+    
+}
+
+.loginInput {
+    width: 100%; 
+    height: 60px; 
+    border-radius: 4px; 
+    margin-top: 8px; 
+    font-family: ${MYFONTS.body}; 
+
+    @media (min-width: 750px)
+    {
+        width: 100%; 
+       
+    }
+}
+
+Button {
+    margin-top: 28px; 
+    width: 100%; 
+    height: 51px; 
+    padding: 13px 133px; 
+    justify-self: center; 
+    background: linear-gradient(90deg, #FF6489 0%, #F9B24E 100%);
+    border-radius: 27px;
+    color: ${COLORS.font.buttonAlternative}; 
+    margin-bottom: 18px; 
+
+    &:focus{
+        background: linear-gradient(90deg, #FF6489 0%, #F9B24E 100%);
+        opacity: 0.8; 
+    }
+
+    @media (min-width: 750px)
+    {
+         
+        align-self: center; 
+        justify-self: center; 
+       
+    }
+}
+
 
 `
