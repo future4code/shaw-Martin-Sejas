ae541f846798f79c449fbada691211accc582abe
martinsejas <martinhugosejas@gmail.com>
2022-05-25 14:31:00 -0300

    Exercicio 2 completo

---
+++ Modulo4/intro-typescript/Exercicio2/src/index.ts
@@ -0,0 +1,7 @@
+
+function imprimeTresCoresFavoritas(cor1:string, cor2:string, cor3:string):void {
+
+    console.log([cor1, cor2, cor3])
+}
+
+imprimeTresCoresFavoritas("Azul", "Verde", "Vermelho"); 
