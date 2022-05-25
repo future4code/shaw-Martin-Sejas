620f4e34face34b5bff085b443141217d6b4b95e
martinsejas <martinhugosejas@gmail.com>
2022-05-25 15:19:59 -0300

    Exercicio 6

---
+++ Modulo4/intro-typescript/Exercicio6/src/index.ts
@@ -0,0 +1,9 @@
+function mathOperations(num1:number, num2:number):void {
+
+    console.log(`A soma de ${num1} e ${num2} é ${num1+num2}`)
+    console.log(`A subtração  de ${num1} e ${num2} é ${num1-num2}`)
+    console.log(`A multiplicação de ${num1} e ${num2} é ${num1+num2}`)
+    num1>num2 ? console.log(`${num1} é maior que ${num2}`):console.log(`${num2} é maior que ${num1}`)  
+}
+
+mathOperations(3,5); 
