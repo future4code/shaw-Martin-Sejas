55d8100b34c2c4d564f75a593b3c47ea2ae02696
martinsejas <martinhugosejas@gmail.com>
2022-05-25 15:01:20 -0300

    Exercicio 4

---
+++ Modulo4/intro-typescript/Exercicio4/src/index.ts
@@ -0,0 +1,24 @@
+function comparaDoisNumeros(num1: number, num2:number):number {
+
+    let maiorNumero:number; 
+    let menorNumero:number; 
+
+    if (num1 > num2) {
+        maiorNumero = num1; 
+        menorNumero = num2;
+    }
+    else {
+        maiorNumero = num2; 
+        menorNumero = num1; 
+    }
+
+    const diferenca:number= maiorNumero - menorNumero; 
+
+    return diferenca; 
+}
+
+//teste 1
+console.log(comparaDoisNumeros(5, 3))
+
+//teste2
+console.log(comparaDoisNumeros(0,5))
