168714bd58d47a947655ed532c2672166bebe708
martinsejas <martinhugosejas@gmail.com>
2022-07-01 20:41:19 -0300

    Trabalhando

--- Modulo5/cookenu/src/index.ts
+++ Modulo5/cookenu/src/index.ts
@@ -1,3 +1,18 @@
 import dotenv from "dotenv"; 
+import {AddressInfo} from "net"; 
+import app from "./app";
 
 dotenv.config();  
+
+//establish endpoints
+
+//server definition
+let server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        let serverAddress = server.address() as AddressInfo; 
+        console.log(`server is listening and serving at ${serverAddress}`);      
+    }
+    else {
+        console.log("Error starting up server")
+    }
+})
---
+++ Modulo5/cookenu/src/types/CookenuUser.ts
@@ -0,0 +1,8 @@
+
+export type CookenuUser = {
+    id:string,
+    name:string, 
+    email:string, 
+    password:string
+
+}
